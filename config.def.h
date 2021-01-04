1f2eb3d2ee69ae793cdc7e7a4162a4d4bc052334
Saverio Turetta <saverioturetta@protonmail.com>
2021-01-04 10:09:34 +0200

    msTeams rule rollback

--- config.def.h
+++ config.def.h
@@ -40,6 +40,7 @@ static const Rule rules[] = {
 	 *	WM_NAME(STRING) = title
 	 */
 	/* class      instance     title       			   tags mask  isfloating  canfocus   monitor */
+        {  NULL,      NULL,        "Microsoft Teams Notification",    0,          1,       0,          -1 },
         {  NULL,      "vimwiki",   NULL,                              SPTAG(0),   1,       1,          -1 },
         {  NULL,      "keepassxc", NULL,                              SPTAG(1),   0,       1,          -1 },
 };
